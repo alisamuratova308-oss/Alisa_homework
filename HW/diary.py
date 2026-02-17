e412eb886f32a6524a9e8091c0b06417355dbe00
Alisa <alisamuratova7@gmail.com>
2026-02-17 17:16:11 +0300

    .

--- HW/diary.py
+++ HW/diary.py
@@ -1,33 +1,52 @@
 class Diary:
     def __init__(self): 
-        self.grades = {}
+        self.__grades = {}
 
     def add_grade(self, subject, grade):
         if subject not in self.grades:  
-            self.grades[subject] = []  
-        self.grades[subject].append(grade) 
+            self.__grades[subject] = []  
+        self.__grades[subject].append(grade) 
         return f"{subject}, {grade}"
     def get_average(self, subject):
-        return sum(self.grades[subject]) / len(self.grades[subject])
+        return sum(self.__grades[subject]) / len(self.__grades[subject])
     def get_all_average(self):
-        oll_grades = self.grades.values()
+        oll_grades = self.__grades.values()
         return sum(oll_grades)/len(oll_grades)
     def get_bad_subjects(self):
+        bad_subjects = []
+        for subject, grades in self.grades.items():
+            if grades and (sum(grades) / len(grades)) < 3.5:
+                bad_subjects.append(subject)
+        return bad_subjects
+    def reset_diary(self):
+        self.__grades = {}
+        return "Оценки очищены "
         
 test = Diary()
 while True:
-    u_input = input("Введите 1) добавить оценку 2)Средний бал 3)средний балл по всем предметам или 'выход' : ")
+    u_input = input("Введите 1) добавить оценку 2)Средний бал 3)средний балл по всем предметам 4)список предметов, где средний балл ниже 3.5 5)очищает весь дневник или 'выход' : ")
     if u_input == "выход":
         break
     elif u_input == "1":
         subject = input("Введите предмет: ")
         grade =float( input("Введите оценку: ")) 
-        print(test.add_grade(subject, grade)) 
+        if subject != '':
+            if 2<=grade<=5 and grade != "":
+                 print(test.add_grade(subject, grade)) 
+        else:
+            print("Неверный ввод")
     elif u_input == "2":
         subject = input("Введите предмет: ")
-        print(test.get_average(subject))
+        if subject != '':
+            print(test.get_average(subject))
+        else:
+            print("Неверный ввод")
     elif u_input == "3":
-        print(test.get_average())
+        print(test.get_all_average())
+    elif u_input == "4":
+        print(test.get_bad_subjects())
+    elif u_input == "5":
+        print(test.reset_diary())
 
 
 
--- HW/test.py
+++ HW/test.py
@@ -1,144 +1,144 @@
-#14.12.25
-#номер 1
-def add_score(scores, name, points):
-    if name in scores:
-        scores[name] += points
-    else:
-        scores[name] = points
-scores = {}
-while True:
-    u_input = input("Введите имя и очки (через пробел) или 'конец':")
-    if u_input.lower() == "конец":  
-        break
-    name, points = u_input.split()  
-    points = int(points)
-    add_score(scores, name, points) 
-print("Результаты:")  
-for name, points in scores.items():  
-    print(f"{name}: {points} очков") 
-max_points = max(scores.values())
-win = []
-for name, points in scores.items():
-    if points == max_points:
-        win.append(name)
-        print(win)
+# #14.12.25
+# #номер 1
+# def add_score(scores, name, points):
+#     if name in scores:
+#         scores[name] += points
+#     else:
+#         scores[name] = points
+# scores = {}
+# while True:
+#     u_input = input("Введите имя и очки (через пробел) или 'конец':")
+#     if u_input.lower() == "конец":  
+#         break
+#     name, points = u_input.split()  
+#     points = int(points)
+#     add_score(scores, name, points) 
+# print("Результаты:")  
+# for name, points in scores.items():  
+#     print(f"{name}: {points} очков") 
+# max_points = max(scores.values())
+# win = []
+# for name, points in scores.items():
+#     if points == max_points:
+#         win.append(name)
+#         print(win)
 
-#номер 2
-def text_pred():
-    text = input("Введите текст: ")
-    text = text.lower()
-    text = text.strip('.,!?;:')
-    text2 = ''
-    for i in text:
-        if i != ",":
-            text2 += i
+# #номер 2
+# def text_pred():
+#     text = input("Введите текст: ")
+#     text = text.lower()
+#     text = text.strip('.,!?;:')
+#     text2 = ''
+#     for i in text:
+#         if i != ",":
+#             text2 += i
 
-    print(text2)
-    words = text2.split()
-    un_words = set(words)
-    print(f"Всего уникальных слов: {len(un_words)}")
-    print("Уникальные слова в тексте:")
-    for words in un_words:
-        print(words,end = ' ')
-text_pred()
+#     print(text2)
+#     words = text2.split()
+#     un_words = set(words)
+#     print(f"Всего уникальных слов: {len(un_words)}")
+#     print("Уникальные слова в тексте:")
+#     for words in un_words:
+#         print(words,end = ' ')
+# text_pred()
 
-#номер 3 н
-contact = {}
-def add_contact(name, phone):
-   contact[name] = phone
-   print(f"Контакт {name} добавлен.")
-def find_contact(contacts,name): 
-   if name in contacts:  
-       print(contacts[name])  
-   else:  
-       print("Контакт не найден.")
-def find_contact_phone(contact,phone):
-    input_phone = contact.values()
-    phone_p = []
-    for i in input_phone:
-        for name, phone in contact.items():
-            if i == phone:
-                phone_p.append(name)
-    print(f"Найдено имя: {phone_p}")
+# #номер 3 н
+# contact = {}
+# def add_contact(name, phone):
+#    contact[name] = phone
+#    print(f"Контакт {name} добавлен.")
+# def find_contact(contacts,name): 
+#    if name in contacts:  
+#        print(contacts[name])  
+#    else:  
+#        print("Контакт не найден.")
+# def find_contact_phone(contact,phone):
+#     input_phone = contact.values()
+#     phone_p = []
+#     for i in input_phone:
+#         for name, phone in contact.items():
+#             if i == phone:
+#                 phone_p.append(name)
+#     print(f"Найдено имя: {phone_p}")
                 
         
 
-while True:  
-    command = input("Введите команду: ").strip().lower()  
+# while True:  
+#     command = input("Введите команду: ").strip().lower()  
       
-    if command == "добавить":  
-        name = input("Введите имя: ").strip()  
-        phone = input("Введите телефон: ").strip()  
-        add_contact(name, phone)  
+#     if command == "добавить":  
+#         name = input("Введите имя: ").strip()  
+#         phone = input("Введите телефон: ").strip()  
+#         add_contact(name, phone)  
           
-    elif command == "найти по имени":  
-        name = input("Введите имя для поиска: ").strip()  
-        find_contact(contact,name)  
-    elif command == "найти по телефону":  
-        phone = input("Введите телефон для поиска: ").strip()  
-        find_contact_phone(contact,phone)
+#     elif command == "найти по имени":  
+#         name = input("Введите имя для поиска: ").strip()  
+#         find_contact(contact,name)  
+#     elif command == "найти по телефону":  
+#         phone = input("Введите телефон для поиска: ").strip()  
+#         find_contact_phone(contact,phone)
           
-    elif command == "выход":    
-        break  
+#     elif command == "выход":    
+#         break  
           
-    else:  
-        print("Неизвестная команда.")  
+#     else:  
+#         print("Неизвестная команда.")  
     
 
-#номер 4 н
+# #номер 4 н
 
-def  count_even_odd(number):
-    word = 0
-    words = 0
-    for numbers in number:
-        if numbers % 2 == 0:
-            word += 1
-        else:
-            words += 1
-    print(f"Четных чисел: {word}, нечетных: {words}")
-def sum_positive_negative(number):
-    num = 0
-    num2 = 0
-    for i in number:
-        if i >0:
-            num += i
-        else:
-            num2 += i
-    print(f"Сумма положительных: {num}, сумма отрицательных: {num2}")
-def find_max_min(number):
-    max_numbers = max(number)
-    min_numbers = min(number)
-    print(f"Максимальное: {max_numbers}, минимальное:{min_numbers}")
-number  = []
-while True:
-    u_input = input("Введите число (или стоп): ")
-    if u_input == "стоп":
-        break
-    numbers = int(u_input)
-    number.append(numbers)
-count_even_odd(number)
-sum_positive_negative(number)
-find_max_min(number)
+# def  count_even_odd(number):
+#     word = 0
+#     words = 0
+#     for numbers in number:
+#         if numbers % 2 == 0:
+#             word += 1
+#         else:
+#             words += 1
+#     print(f"Четных чисел: {word}, нечетных: {words}")
+# def sum_positive_negative(number):
+#     num = 0
+#     num2 = 0
+#     for i in number:
+#         if i >0:
+#             num += i
+#         else:
+#             num2 += i
+#     print(f"Сумма положительных: {num}, сумма отрицательных: {num2}")
+# def find_max_min(number):
+#     max_numbers = max(number)
+#     min_numbers = min(number)
+#     print(f"Максимальное: {max_numbers}, минимальное:{min_numbers}")
+# number  = []
+# while True:
+#     u_input = input("Введите число (или стоп): ")
+#     if u_input == "стоп":
+#         break
+#     numbers = int(u_input)
+#     number.append(numbers)
+# count_even_odd(number)
+# sum_positive_negative(number)
+# find_max_min(number)
 
 
 
-#номер 5 
-def shift_char(text, shift): 
-    for i in range(text_len):
-        code = ord(text[i])
-        if 97 <= code <= 122:
-            print(chr((code - 97 + shift) % 26 + 97),end="")
-        elif 65 <= code <= 90:    
-            print(chr((code - 65 + shift) % 26 + 65),end="") 
-        elif 1072 <= code <= 1103:
-            print(chr((code - 1072 + shift) % 32 + 1072),end="")
-        elif 1040 <= code <= 1071:  
-            print(chr((code - 1040 + shift) % 32 + 1040),end="")
+# #номер 5 
+# def shift_char(text, shift): 
+#     for i in range(text_len):
+#         code = ord(text[i])
+#         if 97 <= code <= 122:
+#             print(chr((code - 97 + shift) % 26 + 97),end="")
+#         elif 65 <= code <= 90:    
+#             print(chr((code - 65 + shift) % 26 + 65),end="") 
+#         elif 1072 <= code <= 1103:
+#             print(chr((code - 1072 + shift) % 32 + 1072),end="")
+#         elif 1040 <= code <= 1071:  
+#             print(chr((code - 1040 + shift) % 32 + 1040),end="")
     
-text = input("Введите текст: ")
-text_len = len(text)
-shift = int(input("Введите сдвиг:")) 
-shift_char(text, shift)
+# text = input("Введите текст: ")
+# text_len = len(text)
+# shift = int(input("Введите сдвиг:")) 
+# shift_char(text, shift)
 
 
 
